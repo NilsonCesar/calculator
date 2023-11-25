722d7ed45249b69732ddfb6e43117ffbfef9edb9
Nilson Cesar <admncoaf@gmail.com>
2023-11-25 18:03:12 -0300

    Create evaluator of arithmethic expressions

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let getDisplayExp = () => display.textContent;
 let validArithmethic = (op) => {
     let displayExp = getDisplayExp();
     let lastIdx = displayExp.length - 1;
-    console.log(lastIdx, op);
     if(lastIdx == -1 && op == '-') {
         return true;
     } else if(lastIdx == -1) {
@@ -18,6 +17,17 @@ let validArithmethic = (op) => {
 }
 let isNumber = op => op in ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+
+let evalExp = (a, b, op) => {
+    if(op == '+') return toString(parseFloat(a) + parseFloat(b));
+    if(op == '*') return toString(parseFloat(a) * parseFloat(b));
+    if(op == '/' && b != '0') return toString(parseFloat(a) / parseFloat(b));
+    else if(op == '/') alert("Impossible division by zero");
+    if(op == '-') {
+        if(a == '') return toString(-1 * parseFloat(b));
+        else return toString(parseFloat(a) - parseFloat(b));
+    }
+}
 
 function updateExp(op) {
     if(op == 'Clear') {
